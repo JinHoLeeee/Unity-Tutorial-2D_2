5b071d68828741052a0704da480cdda800e07235
JeongDay <cidd1332@gmail.com>
2025-06-24 17:30:49 +0900

    2025.06.24 화요일 - 오후시간 3
    공격과 콤보 코드 수정

--- Assets/02. Scripts/Knight/KnightController_Joystick.cs
+++ Assets/02. Scripts/Knight/KnightController_Joystick.cs
@@ -1,114 +1,108 @@
 using System;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class KnightController_Joystick : MonoBehaviour
-{
-    private Animator animator;
-    private Rigidbody2D knightRb;
-
-    [SerializeField] private Button jumpButton;
-    [SerializeField] private Button atkButton;
-
-    private Vector3 inputDir;
-    [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] private float jumpPower = 13f;
-
-    private bool isGround = false;
-    private bool isAttack = false;
-    private bool isCombo = false;
-
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        knightRb = GetComponent<Rigidbody2D>();
-
-        jumpButton.onClick.AddListener(Jump);
-        atkButton.onClick.AddListener(Attack);
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void FixedUpdate()
-    {
-        Move();
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            animator.SetBool("isGround", true);
-            isGround = true;
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            animator.SetBool("isGround", false);
-            isGround = false;
-        }
-    }
-
-    public void InputJoystick(float x, float y)
-    {
-        inputDir = new Vector3(x, y, 0).normalized;
-        
-        animator.SetFloat("JoystickX", inputDir.x);
-        animator.SetFloat("JoystickY", inputDir.y);
-
-        if (inputDir.x != 0)
-        {
-            var scaleX = inputDir.x > 0 ? 1 : -1;
-            transform.localScale = new Vector3(scaleX, 1, 1);
-        }
-    }
-
-    void Move()
-    {
-        if (inputDir.x != 0)
-            knightRb.linearVelocityX = inputDir.x * moveSpeed;
-    }
-
-    void Jump()
-    {
-        if (isGround)
-        {
-            animator.SetTrigger("Jump");
-            knightRb.AddForceY(jumpPower, ForceMode2D.Impulse);
-        }
-    }
-
-    void Attack()
-    {
-        if (!isAttack)
-        {
-            isAttack = true;
-            animator.SetTrigger("Attack");
-        }
-        else
-        {
-            isCombo = true;
-            Debug.Log("콤보 확인");
-        }
-    }
-
-    public void CheckCombo()
-    {
-        if (isCombo)
-        {
-            Debug.Log("콤보 실행");
-            animator.SetBool("isCombo", true);
-        }
-        else
-        {
-            animator.SetBool("isCombo", false);
-            isAttack = false;
-        }
-    }
-}
+ using UnityEngine;
+ using UnityEngine.UI;
+ 
+ public class KnightController_Joystick : MonoBehaviour
+ {
+     private Animator animator;
+     private Rigidbody2D knightRb;
+ 
+     [SerializeField] private Button jumpButton;
+     [SerializeField] private Button atkButton;
+ 
+     private Vector3 inputDir;
+     [SerializeField] private float moveSpeed = 3f;
+     [SerializeField] private float jumpPower = 13f;
+ 
+     private bool isGround = false;
+     private bool isAttack = false;
+ 
+     void Start()
+     {
+         animator = GetComponent<Animator>();
+         knightRb = GetComponent<Rigidbody2D>();
+ 
+         jumpButton.onClick.AddListener(Jump);
+         atkButton.onClick.AddListener(Attack);
+     }
+ 
+     void Update()
+     {
+         
+     }
+ 
+     void FixedUpdate()
+     {
+         Move();
+     }
+ 
+     void OnCollisionEnter2D(Collision2D other)
+     {
+         if (other.gameObject.CompareTag("Ground"))
+         {
+             animator.SetBool("isGround", true);
+             isGround = true;
+         }
+     }
+ 
+     void OnCollisionExit2D(Collision2D other)
+     {
+         if (other.gameObject.CompareTag("Ground"))
+         {
+             animator.SetBool("isGround", false);
+             isGround = false;
+         }
+     }
+ 
+     public void InputJoystick(float x, float y)
+     {
+         inputDir = new Vector3(x, y, 0).normalized;
+         
+         animator.SetFloat("JoystickX", inputDir.x);
+         animator.SetFloat("JoystickY", inputDir.y);
+     }
+ 
+     void Move()
+     {
+         if (inputDir.x != 0)
+         {
+             var scaleX = inputDir.x > 0 ? 1 : -1;
+             transform.localScale = new Vector3(scaleX, 1, 1);
+             
+             knightRb.linearVelocityX = inputDir.x * moveSpeed;
+         }
+     }
+ 
+     void Jump()
+     {
+         if (isGround)
+         {
+             animator.SetTrigger("Jump");
+             knightRb.AddForceY(jumpPower, ForceMode2D.Impulse);
+         }
+     }
+ 
+     void Attack()
+     {
+         animator.SetBool("isCombo", false);
+         
+         if (!isAttack)
+         {
+             isAttack = true;
+             animator.SetTrigger("Attack");
+         }
+         else
+             animator.SetBool("isCombo", true);
+     }
+ 
+     // public void CheckCombo()
+     // {
+     //     if (!animator.GetBool("isCombo"))
+     //         isAttack = false;
+     // }
+ 
+     public void EndAttackCombo()
+     {
+         isAttack = false;
+     }
+ }
