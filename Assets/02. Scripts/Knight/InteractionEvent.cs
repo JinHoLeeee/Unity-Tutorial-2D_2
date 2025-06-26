dedc758391aa0fc38d544b37d0caafa5e2c71a7d
JeongDay <cidd1332@gmail.com>
2025-06-26 16:00:13 +0900

    2025.06.26 목요일 - 오후시간
    NPC
    집 안에 들어가기

--- Assets/02. Scripts/Knight/InteractionEvent.cs
+++ Assets/02. Scripts/Knight/InteractionEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class InteractionEvent : MonoBehaviour
@@ -6,7 +7,15 @@ public class InteractionEvent : MonoBehaviour
     public enum InteractionType { SIGN, DOOR, NPC }
     public InteractionType type;
 
-    public GameObject signPopup;
+    public GameObject popUp;
+
+    public FadeRoutine fade;
+    
+    public GameObject map;
+    public GameObject house;
+
+    public Vector3 inDoorPos;
+    public Vector3 outDoorPos;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,19 +25,40 @@ public class InteractionEvent : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            popUp.SetActive(false);
+        }
+    }
+
     void Interaction(Transform player)
     {
         switch (type)
         {
             case InteractionType.SIGN:
-                signPopup.SetActive(true);
+                popUp.SetActive(true);
                 break;
             case InteractionType.DOOR:
-                
+                StartCoroutine(DoorRoutine(player));
                 break;
             case InteractionType.NPC:
-                
+                popUp.SetActive(true);
                 break;
         }
+    }
+
+    IEnumerator DoorRoutine(Transform player)
+    {
+        yield return StartCoroutine(fade.Fade(3f, Color.black, true));
+
+        player.transform.position = inDoorPos;
+        map.SetActive(false);
+        house.SetActive(true);
+        
+        yield return new WaitForSeconds(1f);
+        
+        yield return StartCoroutine(fade.Fade(3f, Color.black, false));
     }
 }
